aac00410a67330574797b3c0bd6c6b8a32748fc4
Bryan <outzz.dev@gmail.com>
2025-09-23 20:00:33 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("ta rodabdo")
+})
+
+import { Board, Led } from "johnny-five";
+const board = new Board();
+
+
+
+
+
+app.get("/piscar", (req, res) => {
+    board.on("ready", () => {
+        console.log("Placa conectada!");
+
+        // Define o LED no pino 13
+        const led = new Led(13);
+        led.blink(500);
+    });
+
+});
