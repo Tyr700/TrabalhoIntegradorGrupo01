890eed217247a3f1227c5242301c432647c3c3c7
Tiago <tlolivira970@gmail.com>
2025-10-24 21:51:38 -0300

    feat: maked npm install and base conection

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from 'cors';
 import { CarController } from "./controller/CarController";
 
 export const app = express();
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true
+}));
 const PORTA = 3000;
 
 app.use(express.json());
